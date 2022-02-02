dafa4708411c7cbee1d6ea84144f4604c6d918d7
AkshitChugh787 <akshit.chugh2001@gmail.com>
2022-02-02 19:08:40 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function getButtonText() {
+//   return "Click on me!";
+// }
+// Create a React Component
+// A component is a function or a class that produces HTML to show the user using jsx, and handles feedback from the user using event handlers
+const App = () => {
+  const buttonText = { text: "Click Me!" };
+  const labelText = "Enter name:";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "tomato", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Take the React Component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
